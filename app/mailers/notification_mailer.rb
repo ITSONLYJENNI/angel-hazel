6cfe9346df09ad2b83ab1191721dba0872a89d34
Jennifer Fernandez <jennifer.f45@yahoo.com>
2019-10-23 01:03:04 +0000

    sending an email

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,12 +1,12 @@
 class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
-  def NotificationMailer.comment_added.deliver(comment)
+  def comment_added(comment)
     @place = comment.place
     @place_owner = @place.user
 
     mail(to: @place_owner.email,
-    subject: "A comment has been added to your place")
+    subject: "A comment has been added to #{@place.name}")
   end
 
 end
