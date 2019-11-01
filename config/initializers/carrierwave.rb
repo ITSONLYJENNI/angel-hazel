c4e96bc76341c0186eacd1e4fc52d0e8f021ffd0
Jennifer Fernandez <jennifer.f45@yahoo.com>
2019-11-01 22:54:27 +0000

    changing AWS

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,8 +5,8 @@ CarrierWave.configure do |config|
   config.fog_public = false
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["AKIA36ZIYTZTGN6E5TVY"],        # required
-    aws_secret_access_key: ENV["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"],        # required
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
+    aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
   }
-  config.fog_directory  = ENV["itsonlyjennibucket"]              # required
+  config.fog_directory  = ENV["AWS_BUCKET"]              # required
 end
